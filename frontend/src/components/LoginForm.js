854adcc13e8417468ba968da29c3213f58f7d1ad
John Peterson <peterson.glock17@gmail.com>
2022-01-06 22:36:16 -0700

    Added fonts

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 function LoginForm() {
     return (
         <div className="form-signin text-center mb-10 w-75 ms-3 me-3">
-            <i className="fas fa-comment-alt fa-3x text-muted mb-3"></i>
+            <i className="fas fa-comment-alt fa-3x text-light-purple mb-3"></i>
             <h1 className="h3 mb-3 fw-normal">Sign In</h1>
             <div className="card p-5 bg-light">
                 <form>
