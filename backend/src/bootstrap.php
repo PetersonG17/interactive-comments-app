903a2673c60ec638c8b2292f0b598e1fd911b92c
John Peterson <peterson.glock17@gmail.com>
2022-05-30 09:59:02 -0600

    Added whoops pretty error package

--- backend/src/bootstrap.php
+++ backend/src/bootstrap.php
@@ -7,6 +7,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->add(new Zeuxisoo\Whoops\Slim\WhoopsMiddleware([
+    'enable' => true,
+    'editor' => 'vscode',
+    'title'  => 'Error Exception',
+]));
+
 include __DIR__ . '/routes/routes.php';
 
 $app->run();
