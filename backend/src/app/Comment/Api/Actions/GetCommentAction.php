8e4ceadaf3559e5e540a4207de0a7cdcbd58363f
John Peterson <peterson.glock17@gmail.com>
2022-09-17 21:32:48 -0600

    Refactors names for docker and adds flyway db migration system

--- backend/src/app/Comment/Api/Actions/GetCommentAction.php
+++ backend/src/app/Comment/Api/Actions/GetCommentAction.php
@@ -2,19 +2,11 @@
 
 namespace App\Comment\Api\Actions;
 
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class GetCommentAction
 {
-    // private $container;
-
-    // public function __construct(ContainerInterface $container)
-    // {
-    //     $this->container = $container;
-    // }
-
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
     {
         dd("Here! Comment");
--- backend/src/bootstrap/app.php
+++ backend/src/bootstrap/app.php
@@ -3,10 +3,14 @@
 use DI\Container;
 use Slim\Factory\AppFactory;
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Database\Migrations\DatabaseMigrationRepository;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// TODO: Clean up this file so that it only creates the application, it will be called by the console and http kernal
+
+$container = new Container();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->add(new Zeuxisoo\Whoops\Slim\WhoopsMiddleware([
@@ -22,24 +26,13 @@ $capsule = new Capsule();
 // TODO: Load this using config files
 $capsule->addConnection([
     'driver' => 'pgsql',
-    'host' => 'interactive-comments-app-postgres',
+    'host' => 'postgres',
     'database' => 'interactive_comments',
     'port' => '5432',
     'username' => 'postgres',
     'password' => 'secret',
 ]);
-
 $capsule->setAsGlobal();
-
-// TODO: Finish this:
-// https://github.com/illuminate/database/blob/master/Migrations/Migrator.php
-// https://github.com/illuminate/database/blob/master/Migrations/DatabaseMigrationRepository.php
-$migrationRepo = new DatabaseMigrationRepository($capsule->getDatabaseManager(), 'migration');
-
-if (!$migrationRepo->repositoryExists()) {
-    $migrationRepo->createRepository();
-}
-
 
 include_once __DIR__ . '/../routes/routes.php';
 
